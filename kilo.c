d3b3b72809627a0f7f19e607ac5a69680f1fa0cf
ParaskItweb <parask.itweb@gmail.com>
2020-12-31 00:59:10 +0530

    Termal screen size access

--- kilo.c
+++ kilo.c
@@ -4,26 +4,40 @@
 #include <termios.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 #define ctrl_key(k) ((k) & 0x1f)
 
 struct 
-termios original_term;
+editorConfig{
+  int screenrows;
+  int screencols;
+  struct termios original_term;
+};
+
+struct editorConfig E;
+void
+die(const char *s){
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  perror(s);
+  exit(1);
+}
 
 void
 disableRawMode(){
-  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_term) == -1){
-    die('tcsetattr');
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.original_term) == -1){
+    die("tcsetattr");
   }
 }
 
 void
 enableRawMode(){
-  if(tcgetattr(STDIN_FILENO, &original_term) == -1){
+  if(tcgetattr(STDIN_FILENO, &E.original_term) == -1){
     die("tcgetattr");
   };
   atexit(disableRawMode);
-  struct termios raw = original_term;
+  struct termios raw = E.original_term;
   
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
@@ -32,37 +46,70 @@ enableRawMode(){
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
   if(tcsetattr(STDIN_FILENO,  TCSAFLUSH, &raw) == -1){
-    die('tcsetattr');
+    die("tcsetattr");
   }
 }
 
-void
+char
 editorReadKey(){
-
+  int nread;
+  char c;
+  while((nread = read(STDIN_FILENO, &c, 1)) != 1){
+    if(nread == -1 && errno != EAGAIN)  die("read");
+  }
+  return c;
 }
 
 void
 editorKeyPress(){
-
+  char c = editorReadKey();
+  switch(c){
+    case ctrl_key('q')  :
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
 }
 
-void
-die(const char *s){
-  perror(s);
-  exit(1);
+void editorDrawRows(){
+  int y;
+  for(y=0; y<E.screenrows; y++){
+    write(STDOUT_FILENO, "~\r\n",3);
+  }
 }
+
+int getWindowSize(int *rows, int *cols){
+  struct winsize ws;
+  if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+    return -1;
+  }
+  else{
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+void initEditor(){
+  if(getWindowSize(&E.screenrows,&E.screencols) == -1) die("Init");
+}
+
+void editorRefreshScreen(){
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[3", 3);
+
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 int
 main() {
   enableRawMode();
-  while (1) {
-    char c = '\0';
-    if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == ctrl_key('q')) break;
+  initEditor();
+  while(1){
+    editorRefreshScreen();
+    editorKeyPress();
   }
   return 0;
 }
